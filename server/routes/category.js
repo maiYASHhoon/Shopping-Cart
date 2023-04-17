6f261d5b6141e08628863432a587e1cd0a9f2055
maiYASHhoon <yash280801@gmail.com>
2023-04-17 14:36:59 +0530

    CRUD APIs for Product and Category

--- server/routes/category.js
+++ server/routes/category.js
@@ -11,6 +11,58 @@ router.post("/add-category", async (req, res) => {
     res.status(400).send(e);
   }
 });
+
+router.get("/view-category", async (req, res) => {
+  try {
+    const myCat = await Category.find();
+    res.status(201).send(myCat);
+  } catch (e) {
+    res.status(400).send(e);
+    //   console.log(e.message);
+  }
+});
+
+router.get("/view-category/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const myCat = await Category.findOne({ _id });
+    if (!myCat) {
+      return res.status(200).send();
+    }
+    res.send(myCat);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.patch("/update-category/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const myCat = await Category.findByIdAndUpdate({ _id });
+    if (!myCat) {
+      return res.status(404).send();
+    }
+    await myCat.save();
+    res.send(myCat);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/delete-category/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const myCat = await Category.findOneAndDelete({ _id });
+
+    if (!myCat) {
+      return res.status(404).send();
+    }
+    res.send(myCat);
+  } catch (e) {
+    res.send(400).send(e);
+  }
+});
 //   router.get("/", (req, res) => res.render("welcome"));
 
 module.exports = router;
--- server/routes/product.js
+++ server/routes/product.js
@@ -11,6 +11,94 @@ router.post("/add-products", async (req, res) => {
     res.status(400).send(e);
   }
 });
+
+router.get("/view-products", async (req, res) => {
+  try {
+    const myPro = await Product.find();
+    res.send(201).send(myPro);
+  } catch (e) {
+    res.status(400).send(e);
+    //   console.log(e.message);
+  }
+});
+
+router.get("/view-products/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const myPro = await Product.findOne({ _id });
+    if (!myPro) {
+      return res.status(200).send();
+    }
+    res.send(myPro);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+/*router.patch("/update-products/:id", async (req, res) => {
+  // const update = Object.keys(req.body);
+  // const allowedUpdates = [" titleProduct", "" ];
+  // const isValid = update.every((update) => allowedUpdates.includes(update));
+
+  // if (!isValid) {
+  //   return res.status(400).send({ error: "Invalid update!" });
+  // }
+  const _id = req.params.id;
+
+  try {
+    const myPro = await Product.findByIdAndUpdate(_id);
+    //   const mycomp = await Company.findOne(_id);
+    if (!myPro) {
+      return res.status(404).send();
+    }
+    //   update.forEach((update) => (mycomp[update] = req.body[update]));
+    //
+    await myPro.save();
+    res.send(myPro);
+    console.log(myPro);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});*/
+
+router.patch("/update-products/:id", async (req, res) => {
+  const update = Object.keys(req.body);
+  const allowedUpdates = ["titleProduct", "amount", "quantity"];
+  const isValid = update.every((update) => allowedUpdates.includes(update));
+
+  if (!isValid) {
+    return res.status(400).send({ error: "Invalid update!" });
+  }
+
+  const _id = req.params.id;
+
+  try {
+    const myPro = await Product.findByIdAndUpdate(_id, req.body, { new: true });
+    if (!myPro) {
+      return res.status(404).send();
+    }
+
+    res.send(myPro);
+    console.log(myPro);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.delete("/delete-products/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const myPro = await Product.findOneAndDelete({ _id });
+
+    if (!myPro) {
+      return res.status(404).send();
+    }
+    res.send(myPro);
+  } catch (e) {
+    res.send(400).send(e);
+  }
+});
 router.get("/", (req, res) => res.render("welcome"));
 
 module.exports = router;
